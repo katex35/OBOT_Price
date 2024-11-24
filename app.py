8da97e4532e54566e0ba9c69d361a56df1f66d1f
Gökay Şahin <katex35@hotmail.com>
2024-11-24 13:47:49 +0300

    os added

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, jsonify
 import requests
 import threading
@@ -56,5 +57,5 @@ if __name__ == "__main__":
     price_thread = threading.Thread(target=fetch_price)
     price_thread.daemon = True
     price_thread.start()
-    app.run(debug=True)
-    #app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    #app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))

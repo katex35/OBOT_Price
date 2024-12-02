66769683554331cbd8e1628e864d594fc66ca893
Gökay Şahin <katex35@hotmail.com>
2024-12-02 19:58:40 +0300

    minor changes

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests
 app = Flask(__name__)
 
 DEXSCREENER_URL = "https://api.dexscreener.com/latest/dex/pairs/solana/7GqEtF893LmBj7XLmpEAswvE6bpoBP4aYTQK9xMrfNwb"
+APT_DEXSCREENER_URL = "https://api.dexscreener.com/latest/dex/tokens/0x8512b34017e087c3707748869ddc317d83f3fe70ab3a162abdc055c761ca9906::OBOT::OBOT"
 
 current_price = 0.0
 previous_price = 0.0
@@ -32,8 +33,37 @@ def fetch_price():
         return None, None, None
     except Exception:
         return None, None, None
+    
+apt_current_price = 0.0
+apt_previous_price = 0.0
+apt_price_change_24h = 0.0
+
+def fetch_apt_price():
+    """Fetch price data from Dexscreener API"""
+    global apt_current_price, apt_previous_price, apt_price_change_24h
+    try:
+        response = requests.get(APT_DEXSCREENER_URL, timeout=10)
+        response.raise_for_status()
+        data = response.json()
+
+        pairs = data.get("pairs", [])
+        if pairs:
+            price = pairs[0].get("priceUsd")
+            price_change = pairs[0].get("priceChange", {}).get("h24", 0.0)
 
 
+            if price:
+                apt_previous_price = apt_current_price
+                apt_current_price = float(price)
+                apt_price_change_24h = float(price_change)
+                return apt_current_price, apt_previous_price, apt_price_change_24h
+
+        return None, None, None
+    except requests.exceptions.RequestException as e:
+        return None, None, None
+    except Exception as e:
+        return None, None, None
+    
 @app.route("/")
 def home():
     """Home page"""
@@ -44,11 +74,15 @@ def home():
 def price():
     """Price endpoint"""
     current, previous, price_change_24h = fetch_price()
-    if current is not None:
+    apt_current, apt_previous, apt_price_change_24h = fetch_apt_price()
+    if current is not None or apt_current is not None:
         return jsonify({
             "currentPrice": current,
             "previousPrice": previous,
-            "priceChange": price_change_24h
+            "priceChange": price_change_24h,
+            "aptCurrentPrice": apt_current,
+            "aptPreviousPrice":  apt_previous,
+            "aptPriceChange": apt_price_change_24h,
         })
     else:
         return jsonify({"error": "Failed to fetch price"}), 500
